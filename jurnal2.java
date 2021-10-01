42a4c7464f85e77e3c8a2ac40c0a64cecac14ca2
zulfimhmad <zulfimbrizaa@gmail.com>
2021-10-01 14:07:14 +0700

    solusi jurnal 02

---
+++ jurnal2.java
@@ -0,0 +1,7 @@
+public class jurnal2 {
+	public static void main (String[] args) {
+		System.out.println ("NIM  : 6706213064");
+		System.out.println ("Nama : Zulfi Muhamad Briza ALghifari");
+		System.out.println ("Kelas: D3IF-45-04");
+	}
+}
